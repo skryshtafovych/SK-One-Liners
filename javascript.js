3d2ddbb52625e8b18f10d06b10d9e1f79fd32803
Stepan <skryshtafovych@gmail.com>
2019-11-15 11:25:59 -0800

    Update javascript.js
    Another One for more coolness

--- javascript.js
+++ javascript.js
@@ -1,2 +1,4 @@
 //Slice and Sort and Array 
 var preSortedWinningNumbers = winningnumbers.slice(0,5).sort(function(a, b){return a-b});
+//Slice Sort Replace Stuff like , for - and Your Imagination 
+var sortedWinningNumbers = winningnumbers.slice(0,5).sort(function(a, b){return a-b}).toString().replace(/,/g, '-');
